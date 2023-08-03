bc5e6a5d24d25e7d2bf1c95feb081574b687db21
Tri Hoang <trihoang09@gmail.com>
2023-08-03 16:57:42 -0600

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
-// Add your code here
+// // Add your code here
+//  function submitData(){
+//     const formData = {
+//         name: " ",
+//         email: " ",
+//     }
+    
+//     const configurationObject = {
+//         method: 'POST',
+//         headers: {
+//             "Content-type" : "application/json",
+//             'Accept' : "application/json",
+//         },
+//         body: JSON.stringify(formData),
+//     }
+//     fetch("http://localhost:3000/users", configurationObject)
+//       .then(function (response) {
+//         return response.json();
+//       })
+//       .then(function (object) {
+//         console.log(object);
+//       })
+//       .catch(function (error) {
+//         alert("Bad things! Ragnarők!");
+//         console.log(error.message);
+//       });
+    
+//   }
+
+function submitData( name, email ) {
+  return fetch( 'http://localhost:3000/users', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify( {
+        name,
+        email
+      } )
+    } )
+    .then( function ( response ) {
+      return response.json()
+    } )
+    .then( function ( object ) {
+      document.body.innerHTML = object[ "id" ]
+    } )
+    .catch( function ( error ) {
+      document.body.innerHTML = error.message
+    } )
+}
